620d0cc6add0712eb72ea0f0c572739f5f95de41
follow2015 <follow2009@foxmail.com>
2016-06-06 17:44:03 +0800

    Update readfile.py
    改进非同一目录的文件读取

--- readfile.py
+++ readfile.py
@@ -1,5 +1,7 @@
+# -*- coding: GB18030 -*-
 import os
 import sys
+import subprocess
 
 
 def readfile(filename):
@@ -9,24 +11,48 @@ def readfile(filename):
 
 
 def chekfile(filename):
-    if not os.path.isfile(filename):
-        print("The file is does not exits")
-        sys.exit(0)
-    elif not os.access(filename, os.R_OK):
+    if not os.access(filename, os.R_OK):
         print("access denied")
         sys.exit(0)
     else:
-        readfile(filename)
+        try:
+            readfile(filename)
+        except UnicodeDecodeError:
+            with open(filename, 'rt', encoding='gb18030') as f:
+                file = f.read()
+                print(file)
+
+
+def find(file):
+    cmd = 'find / -name %s 2>/dev/null' % file
+    h = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    h.wait()
+    g = h.communicate().__str__()
+    myfile = open('test.txt', 'w+')
+    myfile.write(g)
+    myfile.seek(0)
+    f = myfile.readlines()
+    myfile.close()
+    print(f)
+    file = input('Please choose you file path:  ')
+    chekfile(file)
+
+
+def path(filename):
+    if os.path.isfile(filename):
+        chekfile(filename)
+    else:
+        find(filename)
 
 
 def main():
     if len(sys.argv) == 1:
         filename = input("Enter your filename: ")
-        chekfile(filename)
+        path(filename)
     elif len(sys.argv) == 2:
         filename = sys.argv[1]
-        print(os.path.abspath(filename))
-        chekfile(filename)
+        print(os.path.dirname(filename))
+        path(filename)
 
 if __name__ == "__main__":
     main()
